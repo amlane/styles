f0bae1f67338fc622217a0f92147907a9257b1ef
amlane <twelvexstring@gmail.com>
2020-03-23 17:13:09 -0500

    separated example and button into separate component files

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
-import { Button } from "@amlane/styles";
+import { ExampleComponent, Button } from "@amlane/styles";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        {/* <ExampleComponent text="Modern React component module" /> */}
-        <Button text="Hello" style={{ color: "silver", background: "pink" }} />
+        <ExampleComponent text="Modern React component module" />
+        <Button text="Hello" style={{ color: "silver", background: "blue" }} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,31 +3,5 @@ import PropTypes from "prop-types";
 
 import styles from "./styles.css";
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  };
-
-  render() {
-    const { text } = this.props;
-
-    return <div className={styles.test}>Example Component: {text}</div>;
-  }
-}
-
-export class Button extends Component {
-  static propTypes = {
-    text: PropTypes.string,
-    style: PropTypes.object
-  };
-
-  render() {
-    const { text, style } = this.props;
-
-    return (
-      <button className={styles.btn} style={style}>
-        {text}
-      </button>
-    );
-  }
-}
+export { default as ExampleComponent } from "./components/ExampleComponent.react";
+export { default as Button } from "./components/Button.react";
