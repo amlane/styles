49674fb866b862a14ffcd1678f1aaef072346550
amlane <twelvexstring@gmail.com>
2020-03-23 22:26:56 -0500

    abstracted components into separate files and export from index, added styled components and testing in real world app

--- example/src/App.js
+++ example/src/App.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <div>
         <ExampleComponent text="Modern React component module" />
-        <Button text="Hello" style={{ color: "silver", background: "blue" }} />
+        <Button text="Hello" />
       </div>
     );
   }
