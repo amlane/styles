2163121f3e1784c24e6305d02c2d5ef9d4096de8
amlane <twelvexstring@gmail.com>
2020-03-23 22:38:47 -0500

    whoops gotta make sure I add both directories

--- src/components/Button.react.js
+++ src/components/Button.react.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import styles from "../styles.css";
+
+const StyledButton = styled.button`
+  background: orange;
+  color: pink;
+`;
 
 export default class Button extends Component {
   static propTypes = {
@@ -13,9 +19,9 @@ export default class Button extends Component {
     const { text, style } = this.props;
 
     return (
-      <button className={styles.btn} style={style}>
+      <StyledButton className={styles.btn} style={style}>
         {text}
-      </button>
+      </StyledButton>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-
-import styles from "./styles.css";
-
 export { default as ExampleComponent } from "./components/ExampleComponent.react";
 export { default as Button } from "./components/Button.react";
